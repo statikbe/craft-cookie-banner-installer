61707097b71de153e49f8a7f2a6ec052964b0e95
Jan Henckens <jan@henckens.be>
2026-04-30 11:13:35 +0200

    Plugin

---
+++ migrations/RenameCookieBannerHandle.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace craft\contentmigrations;
+
+use Craft;
+use craft\db\Migration;
+use craft\db\Query;
+
+class m260430_094400_rename_cookie_banner_handle extends Migration
+{
+    private const OLD_HANDLE = 'cookie-banner';
+    private const NEW_HANDLE = '_statik-cookie-banner';
+
+    public function safeUp(): bool
+    {
+        $oldDb = (new Query())
+            ->from('{{%plugins}}')
+            ->where(['handle' => self::OLD_HANDLE])
+            ->one();
+
+        $newDb = (new Query())
+            ->from('{{%plugins}}')
+            ->where(['handle' => self::NEW_HANDLE])
+            ->one();
+
+        if ($oldDb && !$newDb) {
+            $this->update(
+                '{{%plugins}}',
+                ['handle' => self::NEW_HANDLE],
+                ['handle' => self::OLD_HANDLE]
+            );
+        }
+
+        $projectConfig = Craft::$app->getProjectConfig();
+        $oldYaml = $projectConfig->get('plugins.' . self::OLD_HANDLE);
+        $newYaml = $projectConfig->get('plugins.' . self::NEW_HANDLE);
+
+        if ($oldYaml && !$newYaml) {
+            try {
+                $projectConfig->set('plugins.' . self::NEW_HANDLE, $oldYaml, force: true);
+                $projectConfig->remove('plugins.' . self::OLD_HANDLE);
+            } catch (\Throwable $e) {
+                Craft::warning(
+                    'cookie-banner handle rename: project config write skipped: ' . $e->getMessage(),
+                    __METHOD__
+                );
+            }
+        }
+
+        return true;
+    }
+
+    public function safeDown(): bool
+    {
+        echo "m260430_094400_rename_cookie_banner_handle cannot be reverted.\n";
+        return false;
+    }
+}
---
+++ src/Plugin.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace statikbe\cookiebanner\installer;
+
+use Composer\Composer;
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Installer\PackageEvent;
+use Composer\Installer\PackageEvents;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    private const TARGET_PACKAGE = 'statikbe/craft-cookie-banner';
+    private const MIGRATION_FILENAME = 'm260430_094400_rename_cookie_banner_handle.php';
+
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            PackageEvents::POST_PACKAGE_INSTALL => 'onPackage',
+            PackageEvents::POST_PACKAGE_UPDATE => 'onPackage',
+        ];
+    }
+
+    public function onPackage(PackageEvent $event): void
+    {
+        $operation = $event->getOperation();
+
+        if ($operation instanceof InstallOperation) {
+            $package = $operation->getPackage();
+        } elseif ($operation instanceof UpdateOperation) {
+            $package = $operation->getTargetPackage();
+        } else {
+            return;
+        }
+
+        if ($package->getName() !== self::TARGET_PACKAGE) {
+            return;
+        }
+
+        $source = dirname(__DIR__) . '/migrations/RenameCookieBannerHandle.php';
+        $destDir = getcwd() . '/migrations';
+        $dest = $destDir . '/' . self::MIGRATION_FILENAME;
+
+        if (!is_file($source)) {
+            $event->getIO()->writeError(sprintf(
+                '<warning>cookie-banner installer: source migration not found at %s</warning>',
+                $source
+            ));
+            return;
+        }
+
+        if (file_exists($dest)) {
+            return;
+        }
+
+        if (!is_dir($destDir) && !@mkdir($destDir, 0755, true) && !is_dir($destDir)) {
+            $event->getIO()->writeError(sprintf(
+                '<warning>cookie-banner installer: could not create %s</warning>',
+                $destDir
+            ));
+            return;
+        }
+
+        if (!@copy($source, $dest)) {
+            $event->getIO()->writeError(sprintf(
+                '<warning>cookie-banner installer: failed to copy migration to %s</warning>',
+                $dest
+            ));
+            return;
+        }
+
+        $event->getIO()->write(sprintf(
+            '<info>cookie-banner installer: installed handle-rename migration at %s</info>',
+            $dest
+        ));
+    }
+}
